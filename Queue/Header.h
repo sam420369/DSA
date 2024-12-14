615410f1660de46e3f91c3069daf09b1a96cb337
Sakshyam Paudel <sakshyampaudel2@gmail.com>
2024-12-14 22:37:57 +0545

    Added some more que

--- Queue/Header.h
+++ Queue/Header.h
@@ -1,6 +1,41 @@
 #ifndef _QUEUE_H_
 #define _QUEUE_H_
 
+#include <iostream>
+#include <vector>
 
+class Queue {
+private:
+	int size = 0;
+	int front = -1;
+	int rear = -1;
+	std::vector<int> q;
+
+public:
+	Queue(int x);
+
+	void enqueue(int num);
+
+	void dequeue();
+
+	void display();
+};
+
+class circularQueue {
+private:
+	int size = 0;
+	int front = 0;
+	int rear = 0;
+	std::vector<int> q;
+
+public:
+	circularQueue(int x);
+
+	void enqueue(int num);
+
+	void dequeue();
+
+	void display();
+};
 
 #endif // !_QUEUE_H_
--- Queue/Source.cpp
+++ Queue/Source.cpp
@@ -1,3 +1,65 @@
 #include <iostream>
 #include "Header.h"
 
+Queue::Queue(int x) : size(x) {
+	q.resize(size);
+}
+
+void Queue::enqueue(int num) {
+	if (this->rear == this->size - 1) {
+		std::cerr << "Queue is full" << std::endl;
+		return;
+	}
+	else {
+		this->rear++;
+		q[this->rear] = num;
+	}
+}
+
+void Queue::dequeue() {
+	if (this->front == this->rear) {
+		std::cerr << "Queue is empty" << std::endl;
+		return;
+	}
+
+	// just use linked list and don't look at the below bs.
+	// lets take a queue like 1 2 3 4 5 6 and so on. the from pointer in pointing at -1 while rear at 7(i.e. number 6). To dequeue we move the front pointer to 0 essentialy ignoring the 1 because the queue always begins at front + 1. 
+
+	front++;
+}
+
+void Queue::display() {
+	for (int it = this->front + 1; it <= rear; it++) {
+		std::cout << q.at(it) << std::endl;
+	}
+}
+
+circularQueue::circularQueue(int x) : size(x) {
+	q.resize(this->size);
+}
+
+void circularQueue::enqueue(int x) {
+	if ((rear + 1) % size == front) {
+		std::cerr << "Queue is full" << std::endl;
+		return;
+	}
+
+	rear = (rear + 1) % size;
+	q[rear] = x;
+}
+
+void circularQueue::dequeue() {
+	if (this->front == this->rear) {
+		std::cerr << "Queue is empty" << std::endl;
+		return;
+	}
+
+	front = (front + 1) % size;
+}
+
+void circularQueue::display() {
+	for (int it = front + 1; it != (rear + 1) % size; it = (it + 1) % size) {
+		std::cout << q[it] << std::endl;
+	}
+
+}
